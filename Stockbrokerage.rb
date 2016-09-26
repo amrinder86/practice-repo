85547e226e74cf30eb46360351f2f908f888c634
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-25 20:40:31 -0700

    Add 2 tables in ruby file

--- Stockbrokerage.rb
+++ Stockbrokerage.rb
@@ -1,2 +1,15 @@
 require 'sqlite3'
 db = SQLite3::Database.new("stockbrokerage.db")
+
+create_table_stocks1 = <<-SQL
+  CREATE TABLE IF NOT EXISTS stocks1(
+    id INTEGER PRIMARY KEY,
+    recommendation VARCHAR(255)
+  )
+SQL
+
+create_table_stocks2 =  <<-SQL
+  CREATE TABLE IF NOT EXISTS stocks2(
+    
+  )
+SQL
