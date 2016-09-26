076a833737fc91da182faecec3dbb4803847dff1
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-26 03:26:59 -0700

    Clean code and add few more comments

--- Stockbrokerage.rb
+++ Stockbrokerage.rb
@@ -29,6 +29,8 @@ db.execute(create_table_stocks2)
 def stocks1_category(db,category)
   db.execute("INSERT INTO stocks1 (recommendation) VALUES (?)",[category])
 end
+
+#Add a stock rating to Database.
 def stocks2_category(db,company,ticker,price,exchange,recomm)
   db.execute("INSERT INTO stocks2 (company_name,
     stock_ticker,
@@ -36,6 +38,7 @@ def stocks2_category(db,company,ticker,price,exchange,recomm)
     stock_exchange,
     recommendation_id) VALUES (?,?,?,?,?)",[company,ticker,price,exchange,recomm])
 end
+
 def introduction(name)
  @name = name
 puts "          Hey #{@name}, Nice to meet you :) \n
@@ -47,48 +50,55 @@ puts "          Hey #{@name}, Nice to meet you :) \n
 * make sure you give your 100 PERCENT on the job.      *\n
 ********************************************************"
 end
+#update a stock rating
 def update_rating (db,stock_ticker,rating)
    db.execute(<<-SQL
-    UPDATE stocks2 SET recommendation_id="#{rating}" Where stock_ticker="#{stock_ticker}";
+    UPDATE stocks2 
+    SET recommendation_id="#{rating}" 
+    Where stock_ticker="#{stock_ticker}";
     SQL
-    )
+  )
  
 end
+# remove a stock from database
 def remove_stock(db,stock_ticker)
   db.execute(<<-SQL
-    DELETE FROM stocks2 Where stock_ticker="#{stock_ticker}";
+    DELETE FROM stocks2 
+    Where stock_ticker="#{stock_ticker}";
     SQL
-    )
+  )
 end
+# display all stock info 
 def full_info(db)
   info = db.execute("SELECT stocks2.company_name,stocks2.stock_ticker,stocks2.stock_price,stocks2.stock_exchange,stocks1.recommendation  FROM stocks2 JOIN stocks1 ON stocks2.recommendation_id = stocks1.id;")
   puts ""
-  p info
-  info.each do |category|
-    puts "
-          Company Name  : #{category['company_name']} 
-          Stock Ticker  : #{category['stock_ticker']}
-          Stock Price   : #{category['stock_price']}
-          Stock Exchange: #{category['stock_exchange']}
-          Rating        : #{category['recommendation']}"
-  
-  end
-  end
+    info.each do |category|
+      puts "
+            Company Name  : #{category['company_name']} 
+            Stock Ticker  : #{category['stock_ticker']}
+            Stock Price   : #{category['stock_price']}
+            Stock Exchange: #{category['stock_exchange']}
+            Rating        : #{category['recommendation']}"
+    
+        end
+
+end
   # View stock rating by stock ticker.
-  def stock_rating_by_ticker(db,stock_ticker)
+def stock_rating_by_ticker(db,stock_ticker)
     info =db.execute(<<-SQL
-    SELECT  stocks1.recommendation FROM stocks2 JOIN stocks1 ON stocks2.recommendation_id = stocks1.id Where stock_ticker="#{stock_ticker}";
+    SELECT  stocks1.recommendation 
+    FROM stocks2 JOIN stocks1 
+    ON stocks2.recommendation_id = stocks1.id 
+    Where stock_ticker="#{stock_ticker}";
     SQL
     )
-     info
     info.each do |category|
     puts " 
           Rating : #{category['recommendation']}"
         end
-
-
-  end
-  def stock_by_price(db,price)
+end
+# view stocks by certain price 
+def stock_by_price(db,price)
     info =db.execute(<<-SQL
     SELECT  stocks2.stock_ticker stocks2.stock_price FROM stocks2 Where stock_price >"#{price}";
     SQL
@@ -100,25 +110,26 @@ def full_info(db)
       Stock Price  : #{category['stock_price']}"
         end
 
-  end
-
-  def less_info(db)
-     info = db.execute("SELECT stocks2.stock_ticker,stocks1.recommendation FROM stocks2 JOIN stocks1 ON stocks2.recommendation_id = stocks1.id;")
-  puts ""
- 
-  info.each do |category|
-    puts " 
+end
+# method to display less info about each stock.
+def less_info(db)
+    info = db.execute("SELECT stocks2.stock_ticker,stocks1.recommendation FROM stocks2 JOIN stocks1 ON stocks2.recommendation_id = stocks1.id;")
+   
+     info.each do |category|
+      puts " 
           Stock Ticker : #{category['stock_ticker']}
           Rating       : #{category['recommendation']}"
         end
-  end
+end
 
 
 #driver code
-#these 3 are categories are mandatory for a firm to rate a stock!
-# stocks1_category(db,"BUY")
-# stocks1_category(db,"HOLD")
-# stocks1_category(db,"SELL")
+# if you download my database file please comment out line 130 to 132.
+#otherwise comment out line 130 to 132 after running the code once.
+#these 3 categories are mandatory for a firm to rate a stock!
+stocks1_category(db,"BUY")
+stocks1_category(db,"HOLD")
+stocks1_category(db,"SELL")
 # stocks2_category(db,"Apple Inc.","AAPL",112.71,"NASDAQ",1)
 
 puts "Please enter your name to start using software:"
@@ -127,7 +138,7 @@ introduction(name)
 loop do
 
 puts""
-puts"What would you like to do.Select number from below:\n
+puts"What would you like to do.Select a number from below:\n
 1 - Add a stock rating to Database.
 2 - Update a stock rating in Database.
 3 - Remove a stock from Database.
@@ -146,37 +157,20 @@ if input == 1
   puts "What is stock's price at the moment?"
   price = gets.chomp.to_i
   puts "What stock exchange the stock trades on like 'NYSE' 'NASDAQ' etc."
-  exchange=gets.chomp
+  exchange=gets.chomp.upcase
   puts "What rating would you recommend for this stock?\n
   type 1 for BUY
   type 2 for HOLD
   type 3 for SELL "
-  # response1 = gets.chomp.to_i
-  # loop do 
-  # if  response1 == 1 || 2 || 3
+  
   recomm=gets.chomp.to_i
-  # break
-  # else 
-  #   puts"Pay Attention!!!
-  #   type 1, 2 or 3"
-  # end
-
-
+ 
   stocks2_category(db,company,ticker,price,exchange,recomm)
 
 elsif input == 2
   # 2 - Update a stock rating in Database.
   less_info(db)
-  # puts "Which stock's rating would you like to Update?"
-  # info = db.execute("SELECT stocks2.stock_ticker,stocks1.recommendation FROM stocks2 JOIN stocks1 ON stocks2.recommendation_id = stocks1.id;")
-  # puts ""
-  # p info
-  # info.each do |category|
-  #   puts " 
-  #         Stock Ticker : #{category['stock_ticker']}
-  #         Rating       : #{category['recommendation']}"
-  
-  # end
+  puts "Which stock's rating would you like to Update?"
   
   puts "Enter Stock Ticker:"
   stock_ticker = gets.chomp.upcase.to_s
@@ -192,21 +186,13 @@ elsif input == 2
 
 elsif input == 3
   puts "Which stock you want to remove from database?"
-    info = db.execute("SELECT stocks2.stock_ticker,stocks1.recommendation FROM stocks2 JOIN stocks1 ON stocks2.recommendation_id = stocks1.id;")
-  puts ""
-  p info
-  info.each do |category|
-    puts " 
-          Stock Ticker : #{category['stock_ticker']}
-          Rating       : #{category['recommendation']}"
-  
-  end
+   less_info(db)
   puts "Enter Stock Ticker To Remove:"
   stock_ticker=gets.chomp.upcase
 
   remove_stock(db,stock_ticker)
 
-  elsif input == 4
+elsif input == 4
     puts "Which stock's Rating you need to check?"
 
     less_info(db)
@@ -216,7 +202,7 @@ elsif input == 3
 
     stock_rating_by_ticker(db,stock_ticker)
     
-    elsif input == 5
+elsif input == 5
       # View stocks by certain price.
       puts "Enter price to see all stocks above that price."
       price=gets.chomp.to_i
@@ -226,8 +212,18 @@ elsif input == 3
 
 elsif input == 6
  full_info(db)
+
 else 
-  puts "#{name}!!!!!Don't sleep on the job!!! PAY ATTENTION!!!!
-  pick a number from 1 to 7."
+  puts "==========================================================="
+  puts "Hey #{name}!!!!!Don't sleep on the job!!! PAY ATTENTION!!!!
+                Wake up and pick a number from 1 to 7 :) "
+  puts "==========================================================="
 end
 end
+puts "==================================================================="
+puts "Now go home,spend some time with family and catch up on some sleep.
+======================Have a wonderful day=========================="
+puts "==================================================================="
+
+# program can use a lot more improvement like create more flexibility with price and update more things like 
+# price,stock exchange  or other categories and handle user input but I'm short on time.sorry :(
