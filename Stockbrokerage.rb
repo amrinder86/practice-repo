cf19dfc6d4e2789c900f3723f290778fbd9f10b5
Amrinder Grewal <grewalamrinder@hotmail.com>
2016-09-25 20:55:28 -0700

    Add things to ask from user in tables

--- Stockbrokerage.rb
+++ Stockbrokerage.rb
@@ -8,8 +8,18 @@ create_table_stocks1 = <<-SQL
   )
 SQL
 
+db.execute(create_table_stocks1)
+
 create_table_stocks2 =  <<-SQL
   CREATE TABLE IF NOT EXISTS stocks2(
-    
+    id INTEGER PRIMARY KEY,
+    company_name VARCHAR(255),
+    stock_ticker VARCHAR(255),
+    stock_price INT,
+    stock_exchange VARCHAR(255),
+    recommendation_id INT,
+    FOREIGN KEY (recommendation_id) REFERENCES stocks1(id));
   )
 SQL
+
+db.execute(create_table_stocks2)
